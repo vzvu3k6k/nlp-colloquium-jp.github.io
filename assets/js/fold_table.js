5786b8c7f03fe8b8ecbf1e63a9761a3682bc3b64
junya-takayama <clps1220@gmail.com>
2024-10-25 16:04:09 +0900

    sessionStorage は文字列しか受け付けないようなので、文字列を渡すように修正

--- assets/js/fold_table.js
+++ assets/js/fold_table.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
+    function toBoolean(booleanStr) {
+        if (booleanStr === null) {
+            return false
+        }
+        return booleanStr.toLowerCase() === "true";
+    }
+
     const entries = window.performance.getEntriesByType('navigation')
     var isBrowserBackUsed = false;
-    var isExpandedPrev = sessionStorage.getItem('isExpanded');
+    var isExpandedPrev = toBoolean(sessionStorage.getItem('isExpanded'));
 
     for (const entry of entries) {
-        if (entry.type === 'back_forward' | entry.type === 'reload') {
+        if (["back_forward", "reload"].includes(entry.type)) {
             isBrowserBackUsed = true;
         }
     }
 
-    if (!isBrowserBackUsed | !isExpandedPrev) {
-        console.log("!");
+    if (!isBrowserBackUsed || !isExpandedPrev) {
         $(".hidden-row").hide();
         $(".expand-button-row").show();
-        sessionStorage.setItem('isExpanded', false);
+        sessionStorage.setItem('isExpanded', 'false');
     };
 
     $("#show-more-btn").click(function(){
         $(".hidden-row").show();
         $(this).hide();
         $(".expand-button-row").hide();
-        sessionStorage.setItem('isExpanded', true);
+        sessionStorage.setItem('isExpanded', 'true');
     });
 });
