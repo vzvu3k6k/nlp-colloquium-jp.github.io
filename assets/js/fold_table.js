7d16cc7300048fb4428ddc458fd55bb847fd85be
junya-takayama <clps1220@gmail.com>
2024-10-25 10:58:35 +0900

    ブラウザバックで状態維持できるようにする

--- assets/js/fold_table.js
+++ assets/js/fold_table.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
-    $(".hidden-row").hide();
-    $(".expand-button-row").show();
+    const entries = window.performance.getEntriesByType('navigation')
+    var isBrowserBackUsed = false;
+    var isExpandedPrev = sessionStorage.getItem('isExpanded');
+
+    for (const entry of entries) {
+        if (entry.type === 'back_forward' | entry.type === 'reload') {
+            isBrowserBackUsed = true;
+        }
+    }
+
+    if (!isBrowserBackUsed | !isExpandedPrev) {
+        console.log("!");
+        $(".hidden-row").hide();
+        $(".expand-button-row").show();
+        sessionStorage.setItem('isExpanded', false);
+    };
 
     $("#show-more-btn").click(function(){
         $(".hidden-row").show();
         $(this).hide();
         $(".expand-button-row").hide();
+        sessionStorage.setItem('isExpanded', true);
     });
 });
